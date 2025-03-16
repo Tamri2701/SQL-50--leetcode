899a6a502abf19e3092ed297dce48d3fc523b641
Tamri2701 <bhardwajtania8438@gmail.com>
2025-03-16 22:26:39 +0530

    added

---
+++ day25.sql
@@ -0,0 +1,40 @@
+/* Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold. */
+# Write your MySQL query statement below
+WITH FirstYear AS (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)
+SELECT s.product_id, f.first_year, s.quantity, s.price
+FROM Sales s
+JOIN FirstYear f 
+ON s.product_id = f.product_id AND s.year = f.first_year;
